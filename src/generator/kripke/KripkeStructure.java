d2bb64e0796c253d20039de00de1f7ba4f8b5991
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-02-13 16:12:31 +0100

    Minor cleanup

--- src/generator/kripke/KripkeStructure.java
+++ src/generator/kripke/KripkeStructure.java
@@ -3,7 +3,6 @@ package generator.kripke;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class KripkeStructure extends ArrayList<KripkeNode> {
