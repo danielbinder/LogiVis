3f7dc2809c4e0659e3386c6bfe9fced63d3d7021
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-02-19 18:32:55 +0100

    Added KripkeStructure.fromString test

--- test/KripkeStructureTest.java
+++ test/KripkeStructureTest.java
@@ -2,6 +2,8 @@ import generator.Generator;
 import generator.kripke.KripkeStructure;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class KripkeStructureTest {
     @Test
     public void testToFormula() {
@@ -9,5 +11,12 @@ public class KripkeStructureTest {
         System.out.println(ks);
         System.out.println(ks.toFormulaString(3));
         System.out.println(ks.toFormula(3));
+    }
+
+    @Test
+    public void testToAndFromString() {
+        KripkeStructure ks = Generator.generateKripkeStructure("4_2_3_1_3_true");
+        String kripkeString = ks.toString();
+        assertEquals(kripkeString, KripkeStructure.fromString(kripkeString).toString());
     }
 }
