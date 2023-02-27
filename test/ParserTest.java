863ad557f9ed96ce76d08f27d8f6019d0369f698
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-02-27 13:09:51 +0100

    Added Parser test cases

--- test/ParserTest.java
+++ test/ParserTest.java
@@ -27,6 +27,30 @@ public class ParserTest {
                      runInput("a | !b & c <-> !(a | b) & c"));
     }
 
+    @Test
+    public void testSingleVariable() {
+        assertEquals(new ActionNode("a"), runInput("a"));
+    }
+
+    @Test
+    public void testNestedNegation() {
+        assertEquals(new NegationNode(new NegationNode(new ActionNode("a"))), runInput("!!a"));
+    }
+
+    @Test
+    public void testDistributive() {
+        assertEquals(new AndNode(new OrNode(new ActionNode("a"), new ActionNode("b")),
+                                 new OrNode(new ActionNode("a"), new ActionNode("c"))),
+                     runInput("(a | b) & (a | c)"));
+    }
+
+    @Test
+    public void testOrderOfOperations() {
+        assertEquals(new OrNode(new AndNode(new ActionNode("a"), new ActionNode("b")),
+                                new AndNode(new ActionNode("c"), new ActionNode("d"))),
+                     runInput("a & b | c & d"));
+    }
+
     private LogicNode runInput(String input) {
         return new Parser().parse(Lexer.tokenize(input));
     }
