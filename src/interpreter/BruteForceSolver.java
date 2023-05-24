92397c854733bde49942aad204d5158e71ba6bfb
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-05-24 14:33:07 +0200

    Cleaned up unused imports

--- src/interpreter/BruteForceSolver.java
+++ src/interpreter/BruteForceSolver.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class BruteForceSolver {
     private final LogicNode formula;
