ae5b93e89ecd0e871f8a3fb17494628123b9f20f
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-06-01 20:59:17 +0200

    Made basic layout and elements

---
+++ visualisation/src/components/FormulaTypeSelection.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+
+export default function FormulaTypeSelection() {
+    const [formulaType, setFormulaType] = useState("boolean")
+
+    return (
+        <fieldset className="fieldset">
+            <legend>&nbsp;Choose a formula type&nbsp;</legend>
+            <input
+                type="radio"
+                id="boolean"
+                name="formulaTypeSelection"
+                value="boolean"
+                checked={formulaType === "boolean"}
+                onChange={() => setFormulaType("boolean")}
+            />
+            <label htmlFor="boolean">Boolean Algebra</label>
+            <input
+                type="radio"
+                id="ctl"
+                name="formulaTypeSelection"
+                value="ctl"
+                checked={formulaType === "ctl"}
+                onChange={() => setFormulaType("ctl")}
+            />
+            <label htmlFor="ctl">CTL Expression</label>
+            <input
+                type="radio"
+                id="regex"
+                name="formulaTypeSelection"
+                value="regex"
+                checked={formulaType === "regex"}
+                onChange={() => setFormulaType("regex")}
+            />
+            <label htmlFor="regex">Regular Expression</label>
+            <input
+                type="radio"
+                id="process"
+                name="formulaTypeSelection"
+                value="process"
+                checked={formulaType === "process"}
+                onChange={() => setFormulaType("process")}
+            />
+            <label htmlFor="process">Process Algebra</label>
+        </fieldset>
+    )
+}
