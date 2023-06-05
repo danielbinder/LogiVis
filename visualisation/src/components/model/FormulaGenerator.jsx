0f1c91ffe4312a333c5b58981470fb82ae0c1357
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-06-05 13:58:52 +0200

    Added 'Operators' field to formula generator

--- visualisation/src/components/model/FormulaGenerator.jsx
+++ visualisation/src/components/model/FormulaGenerator.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 export default function FormulaGenerator() {
     const [generationParameters, setGenerationParameters] = useState({
         variables: 3,
+        operators: 5,
     })
 
     function handleChange({target: {name, value, type, checked}}) {
@@ -23,18 +24,28 @@ export default function FormulaGenerator() {
             <fieldset className="smallFieldset">
                 <legend>&nbsp;Generate a formula&nbsp;</legend>
                 <div>
-                    <div>
-                        <input
-                            className="input"
-                            type="text"
-                            id="variables"
-                            name="variables"
-                            placeholder="Variables"
-                            value={generationParameters.variables}
-                            onChange={handleChange}
-                        />
-                        <label htmlFor="variables">Variables</label>
-                    </div>
+                    <input
+                        className="input"
+                        type="text"
+                        id="variables"
+                        name="variables"
+                        placeholder="Variables"
+                        value={generationParameters.variables}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="variables">Variables</label>
+                </div>
+                <div>
+                    <input
+                        className="input"
+                        type="text"
+                        id="operators"
+                        name="operators"
+                        placeholder="Operators"
+                        value={generationParameters.operators}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="operators">Operators</label>
                 </div>
                 <div className="centerContainer">
                     <button className="button" onClick={handleButtonClick}>Generate formula</button>
