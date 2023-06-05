b31f62db252050524dd384ae5261abdcaeeecd68
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-06-05 16:05:33 +0200

    Removed compact checkbox for qbf
    Also changed setFormulaType for encoding when qbf is selected

--- visualisation/src/components/model/Model.jsx
+++ visualisation/src/components/model/Model.jsx
@@ -6,7 +6,7 @@ import FormulaGenerator from "./FormulaGenerator";
 import ModelTypeSelector from "./ModelTypeSelector";
 import AlgorithmTester from "./AlgorithmTester";
 
-export default function Model({setFormulaType, setFormula, setSolutionInfo, model, setModel}) {
+export default function Model({formulaType, setFormulaType, setFormula, setSolutionInfo, model, setModel}) {
     const [modelType, setModelType] = useState("kripke")
     const [graph, setGraph] = useState("")
     const [errorMessage, setErrorMessage] = useState("")
@@ -36,6 +36,7 @@ export default function Model({setFormulaType, setFormula, setSolutionInfo, mode
                 <div className="smallColumn">
                     <FormulaGenerator/>
                     <ModelEncoder
+                        formulaType={formulaType}
                         setFormulaType={setFormulaType}
                         setFormula={setFormula}
                         setSolutionInfo={setSolutionInfo}
