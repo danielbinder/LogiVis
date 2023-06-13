45930c654859077a40621fc92669fbaa67a0c569
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-06-13 11:41:14 +0200

    Refactored naive and compact model encoding to get displayed in formula
    Also changes formula type to boolean if one of those 2 are evaluated

--- visualisation/src/App.jsx
+++ visualisation/src/App.jsx
@@ -3,6 +3,8 @@ import Solver from './components/solver/Solver'
 import Model from "./components/model/Model";
 
 export default function App() {
+    const [formulaType, setFormulaType] = useState("boolean")
+    const [formula, setFormula] = useState("")
     const [solution, setSolution] = useState("")
     const [solutionInfo, setSolutionInfo] = useState("")
     const [model, setModel] = useState("")
@@ -10,6 +12,8 @@ export default function App() {
     return (
         <div>
             <Solver
+                formula={formula}
+                setFormula={setFormula}
                 solution={solution}
                 setSolution={setSolution}
                 solutionInfo={solutionInfo}
@@ -17,6 +21,8 @@ export default function App() {
                 model={model}
             />
             <Model
+                setFormulaType={setFormulaType}
+                setFormula={setFormula}
                 setSolution={setSolution}
                 setSolutionInfo={setSolutionInfo}
                 model={model}
