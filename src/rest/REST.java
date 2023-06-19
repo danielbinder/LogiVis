1b309fbc4b26558c0221179c522cf30a1c4c0a60
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-06-19 19:12:10 +0200

    Refactored REST API to stream

--- src/rest/REST.java
+++ src/rest/REST.java
@@ -2,7 +2,7 @@ package rest;
 
 import servlet.Servlet;
 
-import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.TreeMap;
 
 import static spark.Spark.get;
@@ -15,17 +15,15 @@ public class REST {
     public static void start(int port) {
         spark.Spark.port(port);
 
-        Servlet servlet = new Servlet();        // servlet is hard coded as target
+        Servlet target = new Servlet();        // servlet is hard coded as target
 
-        for(Method m : servlet.getClass().getDeclaredMethods()) {
-            if(m.isAnnotationPresent(GET.class)) {
-                get(m.getAnnotation(GET.class).value(),
-                    (req, res) -> {
-                    res.type("application/json");
-                    res.header("Access-Control-Allow-Origin", "*");     // prevents CORS errors
-                    return m.invoke(servlet, new TreeMap<>(req.params()).values().toArray());
-                });
-            }
-        }
+        Arrays.stream(target.getClass().getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(GET.class))
+                .forEach(m -> get(m.getAnnotation(GET.class).value(),
+                             (req, res) -> {
+                                 res.type("application/json");
+                                 res.header("Access-Control-Allow-Origin", "*");     // prevents CORS errors
+                                 return m.invoke(target, new TreeMap<>(req.params()).values().toArray());
+                             }));
     }
 }
