f289899aac127378e41da3285fe664c33fce04f0
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-06-26 10:10:46 +0200

    Refactored Servlet to utilise new convenience Result Methods

--- src/servlet/Servlet.java
+++ src/servlet/Servlet.java
@@ -1,8 +1,8 @@
 package servlet;
 
 import generator.Generator;
-import kripke.KripkeStructure;
-import kripke.KripkeTruthTable;
+import model.kripke.KripkeStructure;
+import model.kripke.KripkeTruthTable;
 import interpreter.BruteForceSolver;
 import interpreter.Simplification;
 import lexer.Lexer;
@@ -29,7 +29,7 @@ public class Servlet {
 
     @GET("/solve/:formula")
     public String solve(String formula) {
-        return resultToJSON(BruteForceSolver.solve(PARSER.parse(Lexer.tokenize(formula))));
+        return BruteForceSolver.solveWithResult(formula).computeJSON();
     }
 
     @GET("/solveCTL/:formula/:model")
@@ -46,20 +46,19 @@ public class Servlet {
 
     @GET("/solveAll/:formula")
     public String solveAll(String formula) {
-        return resultToJSON(BruteForceSolver.solveAll(PARSER.parse(Lexer.tokenize(formula))));
+        return BruteForceSolver.solveAllWithResult(formula).computeJSON();
     }
 
     @GET("/generate/:params")
     public String generate(String params) {
-        return resultToJSON(Map.of("result", Generator.generateKripkeStructure(params, 10).toString()));
+        return Generator.generateKripkeStructureWithResult(params, 10).computeJSON();
     }
 
     @GET("/kripke2formula/:kripke/:steps")
     public String kripke2formula(String kripke, String steps)   {
         String rawKripke = kripke.replace(",", ";");
-        return resultToJSON(Map.of("result", KripkeStructure.fromString(rawKripke)
-                                                            .toFormulaString(Integer.parseInt(steps))
-                                                            .replaceAll("\n", " ")));
+        return KripkeStructure.fromString(rawKripke)
+                .toFormulaStringWithResult(Integer.parseInt(steps)).computeJSON();
     }
 
     @GET("/kripke2compactFormula/:kripke/:steps")
@@ -94,7 +93,7 @@ public class Servlet {
 
     @GET("/simplify/:formula")
     public String simplify(String formula) {
-        return resultToJSON(Map.of("result", Simplification.of(PARSER.parse(Lexer.tokenize(formula))).toString()));
+        return Simplification.ofWithResult(formula).computeJSON();
     }
 
     private static String resultToJSON(List<Map<String, String>> assignments) {
