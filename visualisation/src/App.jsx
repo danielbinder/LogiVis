10576cb7d2c74f05ed95c99752fcfe2daca16363
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-08-09 15:12:01 +0200

    Removed unnecessary logging

--- visualisation/src/App.jsx
+++ visualisation/src/App.jsx
@@ -34,7 +34,6 @@ export default function App() {
     }
 
     const setModelTab = (data) => {
-        console.log(data)
         setModel(data['result']
             .replaceAll(/[$]/g, '\n')
             .replaceAll(/_/g, ';')
