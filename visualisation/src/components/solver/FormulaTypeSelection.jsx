94e9f604aad89a14f4f6b2892b68d636672f7e0c
danielbinder <59804199+danielbinder@users.noreply.github.com>
2023-09-26 15:49:05 +0200

    Changed double quote to single quote in FormulaTypeSelector

--- visualisation/src/components/solver/FormulaTypeSelection.jsx
+++ visualisation/src/components/solver/FormulaTypeSelection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useRecoilState} from "recoil";
+import {useRecoilState} from 'recoil';
 import {formulaTypeState} from '../atoms';
 
 export default function FormulaTypeSelection() {
