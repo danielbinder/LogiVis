c505f13d5cff2506482cbb0f25d942abad21f070
Christoph Steidl <christoph.steidl@gmail.com>
2023-09-28 14:24:33 +0200

    some change in class ServletTest.java

--- test/ServletTest.java
+++ test/ServletTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import servlet.Servlet;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URI;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -23,20 +25,22 @@ public class ServletTest {
 
     @Test
     public void testParenthesise() {
-        testResult("a & (b | c)", "http://localhost:4000/parenthesise/(a&(b|c))");
+        testResult("a & (b | c)", "http://localhost:4000/parenthesise/(a&(b%7Cc))");
     }
 
     private void testResult(String expected, String url) {
         try {
-            HttpURLConnection connection = (HttpURLConnection) URI.create(url).toURL().openConnection();
-            connection.setRequestMethod("GET");
-            try(Scanner s = new Scanner(connection.getInputStream())) {
+            URL endpointUrl = new URL(url);
+            URLConnection connection = endpointUrl.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            try(Scanner s = new Scanner(in)) {
                 s.nextLine();   // Read over initial {\n of JSON
 
                 assertEquals("\t\"result\": \"" + expected + "\",", s.nextLine());
             }
-        } catch(IOException e) {
-            throw new RuntimeException(e);
+        } catch(IOException ex) {
+            throw new RuntimeException(ex);
         }
     }
 }
