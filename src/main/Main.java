7563207b68ed08a6d55da98e8febe651e2822d9a
Christoph Steidl <christoph.steidl@gmail.com>
2023-10-19 18:56:21 +0200

    Added class FrontendExtractor.java  - This class is used to extract the production build of the frontend from a shadow JAR to allow Spark to automatically serve it via a static external location.  - The extractor only becomes active, if the "-extract" parameter has been provided for the main class (src/main/Main.java).

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,15 @@ package main;
 import algorithmTester.AlgorithmTester;
 import servlet.Servlet;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 public class Main {
     public static void main(String[] args) {
+        if(args.length > 0 && FrontendExtractor.CLI_ARG_EXTRACT.equals(args[0])) FrontendExtractor.extract();
         Servlet.main(args);
         AlgorithmTester.main(args);
     }
