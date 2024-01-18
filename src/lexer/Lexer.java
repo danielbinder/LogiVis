33ced7e782db2653d9b48215afeda58e7c10799e
danielbinder <59804199+danielbinder@users.noreply.github.com>
2024-01-18 18:16:37 +0100

    Extracted Lexer one lexer out of both (API remains unchanged)
    Boolean formulas would now be able to parse strings enclosed in apostrophes if apostrophes were valid characters

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -2,54 +2,33 @@ package lexer;
 
 import bool.token.BooleanToken;
 import bool.token.BooleanTokenType;
+import marker.Token;
 import model.token.ModelToken;
 import model.token.ModelTokenType;
+import util.Error;
+import util.TriFunction;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.function.Predicate;
 
 public class Lexer {
     public static List<BooleanToken> tokenizeBooleanFormula(final String input) {
-        int line = 1;
-        int col = 0;
-        final List<BooleanToken> booleanTokens = new ArrayList<>();
-        StringBuilder current = new StringBuilder();
-
-        for(int i = 0; i < input.length(); i++) {
-            col++;
-            if(Character.isWhitespace(input.charAt(i))) {
-                if(input.charAt(i) == '\n') {
-                    line++;
-                    col = 0;
-                }
-
-                continue;
-            }
-
-            if(!BooleanTokenType.isValidCharacter(input.charAt(i))) error(input, line, col);
-            current.append(input.charAt(i));
-
-            if((Character.isAlphabetic(input.charAt(i)) || Character.isDigit(input.charAt(i))) &&
-                    i + 1 < input.length() &&
-                    (Character.isAlphabetic(input.charAt(i + 1)) || Character.isDigit(input.charAt(i + 1)))) continue;
-
-            try {
-                booleanTokens.add(BooleanToken.fromString(current.toString(), line, col));       // this is where the magic happens
-                current = new StringBuilder();
-            } catch(NoSuchElementException ignored) {}
-        }
-
-        booleanTokens.add(BooleanToken.fromString("EOF", line, col));
-
-        return booleanTokens;
+        return Lexer.tokenize(input, BooleanTokenType::isValidCharacter, BooleanToken::fromString);
     }
 
     public static List<ModelToken> tokenizeModel(final String input) {
-        boolean openString = false;
+        return Lexer.tokenize(input, ModelTokenType::isValidCharacter, ModelToken::fromString);
+    }
+
+    private static <T extends Token> List<T> tokenize(String input,
+                                                     Predicate<Character> characterValidator,
+                                                     TriFunction<String, Integer, Integer, T> tokenCreator) {
         int line = 1;
         int col = 0;
-        final List<ModelToken> modelTokens = new ArrayList<>();
+        boolean openString = false;
+        final List<T> tokens = new ArrayList<>();
         StringBuilder current = new StringBuilder();
 
         for(int i = 0; i < input.length(); i++) {
@@ -71,7 +50,7 @@ public class Lexer {
 
             // Take anything inside String
             if(input.charAt(i) == '\'') openString = !openString;
-            if(!openString && !ModelTokenType.isValidCharacter(input.charAt(i))) error(input, line, col);
+            if(!openString && !characterValidator.test(input.charAt(i))) error(input, line, col);
             current.append(input.charAt(i));
 
             if((Character.isAlphabetic(input.charAt(i)) || Character.isDigit(input.charAt(i))) &&
@@ -79,26 +58,23 @@ public class Lexer {
                     (Character.isAlphabetic(input.charAt(i + 1)) || Character.isDigit(input.charAt(i + 1)))) continue;
 
             try {
-                if(input.charAt(i) == '-' && i + 1 < input.length() && input.charAt(i + 1) == '>') continue;
-                modelTokens.add(ModelToken.fromString(current.toString(), line, col));       // this is where the magic happens
+                try {
+                    tokenCreator.apply(current.toString() + input.charAt(i + 1), line, col);
+                    continue;
+                } catch(Exception ignored) {}
+                tokens.add(tokenCreator.apply(current.toString(), line, col));       // this is where the magic happens
                 current = new StringBuilder();
             } catch(NoSuchElementException ignored) {}
         }
 
-        modelTokens.add(ModelToken.fromString("EOF", line, col));
+        tokens.add(tokenCreator.apply("EOF", line, col));
 
-        return modelTokens;
+        return tokens;
     }
 
     private static void error(String input, int line, int col) {
         String lineStr = input.split("\n")[line - 1];
-        if(lineStr.length() > 40) {
-            lineStr = lineStr.substring(Math.max(0, col - 40), Math.min(lineStr.length(), col + 40));
-            col = col - 40 < 0 ? col : 40;
-        }
-
-        System.out.println(lineStr);
-        System.out.println(" ".repeat(col - 1) + "^");
+        Error.printPosition(lineStr, col);
         throw new IllegalArgumentException("Illegal character or sequence starting with '" +
                                                    lineStr.charAt(col - 1) +
                                                    "' at [" + line + "|" + col + "]");
