ee23350ae82e704485580990a3f09e6e3cdd05c6
danielbinder <59804199+danielbinder@users.noreply.github.com>
2024-04-16 22:29:23 +0200

    Added CNFSolvers and BooleanConstraintPropergation
    Only skeleton of CDCLSolver exists currently

---
+++ src/main/java/bool/variant/cnf/interpreter/RecursiveDPLLSolver.java
@@ -0,0 +1,28 @@
+package bool.variant.cnf.interpreter;
+
+import bool.variant.cnf.parser.cnfnode.Conjunction;
+import bool.variant.cnf.parser.cnfnode.Variable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public final class RecursiveDPLLSolver implements CNFSolver {
+    public boolean unsatisfiable = false;
+
+    @Override
+    public Map<Variable, Boolean> solve(Conjunction conjunction) {
+        conjunction = BCP(conjunction);
+        if(conjunction.isEmpty()) return conjunction.withRemainingClausesAssignedTrue().assignment;
+        if(conjunction.stream().anyMatch(ArrayList::isEmpty)) {
+            unsatisfiable = true;
+            return Map.of();
+        };
+        List<Variable> variablesInBothPolarities = variablesInBothPolarities(conjunction);
+        if(variablesInBothPolarities.isEmpty()) return conjunction.withRemainingClausesAssignedTrue().assignment;
+        Variable bpVar = variablesInBothPolarities.getFirst();
+        Map<Variable, Boolean> withBPVar = solve(conjunction.clone().withUnitClause(bpVar));
+        if(!withBPVar.isEmpty()) return withBPVar;
+        return solve(conjunction.withUnitClause(bpVar.negated()));
+    }
+}
