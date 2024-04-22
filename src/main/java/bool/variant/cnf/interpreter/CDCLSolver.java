6e1f183cd1c3ccd1f5519e30d1ac7b80ee24467d
danielbinder <59804199+danielbinder@users.noreply.github.com>
2024-04-22 20:53:37 +0200

    Created based CDCLSolver algorithm

--- src/main/java/bool/variant/cnf/interpreter/CDCLSolver.java
+++ src/main/java/bool/variant/cnf/interpreter/CDCLSolver.java
@@ -1,8 +1,10 @@
 package bool.variant.cnf.interpreter;
 
+import bool.variant.cnf.parser.cnfnode.AbstractVariable;
 import bool.variant.cnf.parser.cnfnode.Conjunction;
 import bool.variant.cnf.parser.cnfnode.Variable;
 
+import java.util.List;
 import java.util.Map;
 
 public final class CDCLSolver implements CNFSolver {
@@ -10,6 +12,18 @@ public final class CDCLSolver implements CNFSolver {
 
     @Override
     public Map<Variable, Boolean> solve(Conjunction conjunction) {
-        return null;
+        while(true) {
+            conjunction = BCP(conjunction);
+            if(conjunction.decisionGraph.conflict) {
+                if(conjunction.decisionGraph.isEmpty()) return Map.of();
+                AbstractVariable firstUIP = conjunction.decisionGraph.getFirstAndLastUIP().left;
+                conjunction.backtrackUIP(firstUIP);
+            } else {
+                List<Variable> variablesInBothPolarities = variablesInBothPolarities(conjunction);
+                if(variablesInBothPolarities.isEmpty()) return conjunction.withRemainingClausesAssignedTrue().assignment;
+                Variable bpVar = variablesInBothPolarities.getFirst();
+                conjunction.decisionGraph.decide(bpVar);
+            }
+        }
     }
 }
